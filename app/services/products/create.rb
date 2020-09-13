2f23a54753f6eb92947a58b99e40298ae193f501
Wlademir Renan dos Santos <wlsantos@bionexo.com>
2020-09-13 12:25:08 -0300

    ajust format of file

--- app/services/products/create.rb
+++ app/services/products/create.rb
@@ -7,19 +7,19 @@ module Services
         @cost_price = params[:cost_price]
         @errors = []
       end
-    
+
       attr_accessor :name, :cost_price, :errors
-    
+
       def call
         validate_fields
         ::Product.create(name: @name, cost_price: @cost_price) if @errors.empty?
       end
-    
+
       def validate_fields
-        @errors << "name is required" if @name.blank?
-        @errors << "cost_price is required" if @cost_price.blank?
+        @errors << 'name is required' if @name.blank?
+        @errors << 'cost_price is required' if @cost_price.blank?
       end
-    
+
     end
   end
 end
