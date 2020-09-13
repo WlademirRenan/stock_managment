588d48b73b8f13760350f82340cf9acdc7ab1870
Wlademir Renan dos Santos <wlsantos@bionexo.com>
2020-09-13 18:18:17 -0300

    Add services and model for stock_item

--- app/models/stock_item.rb
+++ app/models/stock_item.rb
@@ -1,2 +1,14 @@
 class StockItem < ApplicationRecord
+  belongs_to :product
+  belongs_to :store
+  def add_quantity(quantity)
+    self.quantity += quantity
+    self.save
+  end
+
+  def remove_quantity(quantity)
+    self.quantity -= quantity
+    self.save
+  end
+
 end
---
+++ app/services/stock_items/remove_quantity.rb
@@ -0,0 +1,33 @@
+module Services
+  module StockItems
+    class RemoveQuantity
+      def initialize(params = {})
+        @product_id = params[:product_id]
+        @store_id = params[:store_id]
+        @quantity = params[:quantity]
+        @errors = []
+      end
+
+      attr_accessor :product_id, :store_id, :quantity, :errors
+
+      def call
+        validate_fields
+        return unless @errors.empty?
+
+        stock = ::StockItem.find_by_product_id_and_store_id(@product_id, @store_id)
+        if stock
+          stock.remove_quantity(@quantity)
+        else
+          @errors << 'stock_item not found'
+        end
+      end
+
+      def validate_fields
+        @errors << 'product_id is required' if @product_id.blank?
+        @errors << 'store_id is required' if @store_id.blank?
+        @errors << 'quantity is required' if @quantity.blank?
+      end
+
+    end
+  end
+end
